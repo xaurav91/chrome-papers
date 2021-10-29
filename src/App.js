e55c2a7b7f2b718f8e3bb6fc5d1847cecf3c19dd
Biju Nakarmi <bezunakarmi@gmail.com>
2021-10-29 10:13:15 +0545

    Added quotes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/searchBar/searchBar';
 import Time from './components/Time';
+import Quotes from './components/Quotes';
+import QuotesService from './services/quotes';
 
 function App() {
-
+  const [quotes, setQuotes] = useState('');
   useEffect(() => {
     document.body.style.backgroundImage = "url('https://picsum.photos/1080')";
+    if (quotes === '') {
+      QuotesService().then(data => {
+        console.log(data);
+        setQuotes(data)
+      }).catch(err => console.log(err));
+    }
   })
   return (
     <div className="App">
       <Time />
       <SearchBar />
+      <Quotes quote={quotes.content} author={quotes.author} />
     </div>
   );
 }
